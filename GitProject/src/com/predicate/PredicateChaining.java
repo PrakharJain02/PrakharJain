6768bc62174c2433e1ad1faa943dca011b40355f
Prakhar02 <prakharjain02@gmail.com>
2020-06-04 14:46:26 +0530

    changes

--- GitProject/src/com/predicate/PredicateChaining.java
+++ GitProject/src/com/predicate/PredicateChaining.java
@@ -16,11 +16,13 @@ public class PredicateChaining {
 		Predicate<Integer> lowestValue  = i -> i < 100;
 		// predicate method calling for test 
 		System.out.println("Greater Than Input Value = " + greaterThan.test(14));
-		System.err.println("Lowest Than Input Value = " + lowestValue.test(14));
+		System.out.println("Lowest Than Input Value = " + lowestValue.test(14));
 		
 		// Predicate Chaining 
 		System.out.println("Predicate Chaining = " + greaterThan.and(lowestValue).test(20));
 		System.out.println("negate() = " + greaterThan.and(lowestValue).negate().test(4));
+		System.out.println("Predicate Chaining = " + greaterThan.or(lowestValue).test(20));
+		
 		
 	}
 
